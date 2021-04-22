64ca90c86968090271ae7748382af9ac8b7cb7cb
gogokgc <gogokgc@gmail.com>
2021-04-22 23:57:59 +0900

    Model - Board edit Table connection

--- src/main/java/com/kgc/blog/model/Board.java
+++ src/main/java/com/kgc/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.kgc.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -39,9 +42,12 @@ public class Board {
 	@ColumnDefault("0")
 	private int count; // 조회수
 	
-	@ManyToOne  // Many = Board, One = User  -- 한명의 유저는 여러 글을 작성할수있다.
+	@ManyToOne(fetch = FetchType.EAGER)  // Many = Board, One = User  -- 한명의 유저는 여러 글을 작성할수있다.
 	@JoinColumn(name = "userId") // 컬럼명은 userId 로 하겠다는 설정
 	private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다. 
+	
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다 (난 FK가 아니에요) DB에 칼럼을 만들지 마세요.
+	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
