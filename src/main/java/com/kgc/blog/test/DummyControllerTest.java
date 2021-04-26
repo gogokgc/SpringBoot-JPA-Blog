a8d6369b1859688d303483eff766604f04b00194
gogokgc <gogokgc@gmail.com>
2021-04-26 17:11:06 +0900

    Test - delete function add

--- src/main/java/com/kgc/blog/test/DummyControllerTest.java
+++ src/main/java/com/kgc/blog/test/DummyControllerTest.java
@@ -6,10 +6,12 @@ import java.util.function.Supplier;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,28 +29,43 @@ public class DummyControllerTest {
 
 	@Autowired // 의존성 주입(DI)
 	private UserRepository userRepository;
-	
+
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return "delete Failed the ID was not exist in DB";
+		}
+
+		return "delete complete id : " + id;
+	}
+
 	// save함수는 id를 전달하지 않으면 insert를 해주고
 	// save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
 	// save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해요.
 	// email, password
 	@Transactional // 함수 종료시에 자동 commit 이 됨.
 	@PutMapping("/dummy/user/{id}")
-	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { //@RequestBody는 클라이언트가 전송하는 Json(application/json) 형태의 HTTP Body 내용을 Java Object로 변환시켜주는 역할(message Converter 의 jackson 라이브러리)을 한다.
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { // @RequestBody는 클라이언트가 전송하는
+																					// Json(application/json) 형태의 HTTP
+																					// Body 내용을 Java Object로 변환시켜주는
+																					// 역할(message Converter 의 jackson
+																					// 라이브러리)을 한다.
 		System.out.println("id : " + id);
 		System.out.println("password : " + requestUser.getPassword());
 		System.out.println("email : " + requestUser.getEmail());
-		
-		User user = userRepository.findById(id).orElseThrow(()->{
+
+		User user = userRepository.findById(id).orElseThrow(() -> {
 			return new IllegalArgumentException("Update Failed");
 		});
 		user.setPassword(requestUser.getPassword());
 		user.setEmail(requestUser.getEmail());
-		
+
 //		requestUser.setId(id);
 //		userRepository.save(user);
 		// 더티체킹 // @Transactional 이 종료될때 데이터의 변경을 감지하여 DB에 수정을 해준다.
-		return null;
+		return user;
 	}
 
 	@GetMapping("/dummy/users")
@@ -59,9 +76,10 @@ public class DummyControllerTest {
 
 	// 한페이지당 2건에 데이터를 리턴받아 볼 예정
 	@GetMapping("/dummy/user")
-	public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC)Pageable pageable){
+	public List<User> pageList(
+			@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
 		Page<User> pagingUser = userRepository.findAll(pageable);
-		
+
 		List<User> users = pagingUser.getContent(); // findAll 에서 같이 가져오는 데이터중 content 데이터만 수집한다.
 		return users;
 	}
