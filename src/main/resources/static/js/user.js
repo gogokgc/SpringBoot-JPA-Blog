cc9f11a9e50be921ff5f5a3e510af5a4fb3a4c70
gogokgc <gogokgc@gmail.com>
2021-05-06 22:52:14 +0900

    Add Modify memberInfo vol.1

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -4,6 +4,9 @@ let index = {
 			this.save();
 		});
 		
+		$("#btn-update").on("click", ()=>{ // function(){} , ()=>{} this를 바인딩하기 위해서!! 
+			this.update();
+		});
 	},
 	
 	save: function(){
@@ -26,6 +29,31 @@ let index = {
 			data: JSON.stringify(data), //http body 데이터 //위의 let data 는 자바스크립스 오브젝트로 자바에서 이해할수있게 json으로 변형시켜 전달
 			contentType: "application/json; charset=utf-8", // 바디데이터가 어떤 타입인지 (MIME)
 			dataType: "json" // 요청을 서버로 해서 응답이 왔을때 기본적으로 모든것이 문자열 (생긴게 json이라면)=>자바스크립트 오브젝트로 변경
+		}).done(function(resp){
+			// 요청 완료시
+			alert("Member registration complete");
+			console.log(resp);
+			location.href="/";
+		}).fail(function(error){
+			// 요청 실패시
+			alert(JSON.stringify(error));
+		});
+	},
+	
+	update: function(){
+		let data = {
+			id:$("#id").val(),
+			password: $("#password").val(),
+			email: $("#email").val()
+		};
+		
+		$.ajax({
+			// 수행 요청
+			type: "PUT",
+			url: "/user/updateForm",
+			data: JSON.stringify(data), 
+			contentType: "application/json; charset=utf-8", 
+			dataType: "json" 
 		}).done(function(resp){
 			// 요청 완료시
 			alert("Member registration complete");
